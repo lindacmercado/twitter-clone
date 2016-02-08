2ab13bb76764b5b1c053e271e384067c6b0f902f
Linda Mercado <lindacmercado@gmail.com>
2016-02-08 13:24:50 -0700

    update

--- js/animations.js
+++ js/animations.js
@@ -1,34 +1,38 @@
 $(document).ready(function() {
-    
-  $('.tweet-compose').click(function() {
-      $('#new-tweet-textarea').css('height', '5em');
-      $('#tweet-controls').css('display', 'block');
-  });  
+   //Text area click event
+   $('#new-tweet-textarea').click(function() {
+       $(this).css('height', '5em');  //set display height
+       $('#tweet-controls').css('display', 'block');  //set tweet button and char count to display
+   })
    
-  $('#new-tweet-textarea').keyup(function() {
-      var tweetLength = $(this).val().length;
-        $('#char-count').text(140 - tweetLength);
-        
-        if(tweetLength >= 130) {
-            $('#char-count').css('color', 'red');
-        } else {
-            $('#char-count').css('color', '#999');
-        }
-        
-        if(tweetLength > 140) {
-            $('#tweet-submit').attr('disabled', 'disabled');
-        } else {
-            $('#tweet-submit').removeAttr('disabled', 'disabled');
-        } 
-  });
+   //text area typing event
+   $('#new-tweet-textarea').keyup(function() {
+       var tweetLength = $(this).val().length;
+       $('#char-count').text(140 - tweetLength);
+       
+       //set char-count text color to red when drops to 10 chars left
+       if(tweetLength >= 130) {
+           $('#char-count').css('color', 'red');
+       } else {
+           $('#char-count').css('color', '#999');
+       }
+       
+       //disable tweet button when char-count reaches 0
+       if(tweetLength > 140) {
+           $('#tweet-submit').attr('disabled', 'disabled');
+       } else {
+           $('#tweet-submit').removeAttr('disabled', 'disabled');
+       }
+    });
     
+    //tweet events
     $('#tweet-submit').click(function() {
-        var tweet = $('#new-tweet-textarea').val();
-        if(!tweet.length) return;
-        
-        var userName = 'Linda Mercado';
-        
-        var tweetHTML = '<div class="tweet">' +
+    var tweet = $('#new-tweet-textarea').val();
+    if(!tweet.length) return;
+
+    var userName = "Linda Mercado";
+
+    var tweetHTML = '<div class="tweet">' +
                         '<div class="content">' +
                             '<img class="avatar" src="img/alagoon.jpg" />' +
                             '<strong class="fullname">' + userName + '</strong>' +
@@ -38,15 +42,17 @@ $(document).ready(function() {
                                 '<ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul>'
                             + '</div>'
                     + '</div>';
-                    
-         $('#stream').prepend(tweetHTML);
-         
-         $('#new-tweet-textarea').val(' ');
-         $('#char-count').text(140);
-    });
-    
-    $('.tweet').click(function() {
-        $(this).find('.stats, .reply').slideDown('fast');
-    });
-    
+
+    $('#stream').prepend(tweetHTML);
+//Reset textarea and char-count
+    $('#new-tweet-textarea').val('');
+    $('#char-count').text(140);
+  });
+//tweet action
+  $('.tweet').click(function() {
+    $(this).find('.stats, .reply').slideDown('fast');
+  })
+  
+  //set timestamp format and automatic
+  jQuery("time.timeago").timeago();
 });
